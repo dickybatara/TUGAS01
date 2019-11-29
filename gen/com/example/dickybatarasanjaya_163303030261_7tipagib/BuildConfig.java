369518d0f5e1407e1a9810be519bd915c894aacd
dickybatara <dickybastara47@gmail.com>
2019-11-29 09:45:54 +0700

    tugas 1

---
+++ gen/com/example/dickybatarasanjaya_163303030261_7tipagib/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.dickybatarasanjaya_163303030261_7tipagib;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
